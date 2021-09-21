278918ef9910fd2a7bd7bfb03062bf0c33697005
Ambroise Maupate <ambroise@rezo-zero.com>
2021-09-21 16:57:36 +0200

    Moved configuration to compiler passes

---
+++ src/DependencyInjection/Compiler/RozierPathsCompilerPass.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+
+namespace RZ\Roadiz\RozierBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\Asset\PathPackage;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Finder\Finder;
+
+class RozierPathsCompilerPass implements CompilerPassInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->hasDefinition('translator.default')) {
+            $this->registerThemeTranslatorResources($container);
+        }
+    }
+
+    private function registerThemeTranslatorResources(ContainerBuilder $container): void
+    {
+        $projectDir = $container->getParameter('kernel.project_dir');
+
+        /*
+         * add Assets package '%kernel.project_dir%/themes/Rozier/static'
+         */
+        $name = 'Rozier';
+        // Register asset packages
+        $container->setDefinition(
+            'roadiz_rozier.assets._package.' . $name,
+            (new Definition())
+                ->setClass(PathPackage::class)
+                ->setArguments([
+                    'themes/' . $name . '/static',
+                    new Reference('assets.empty_version_strategy'),
+                    new Reference('assets.context')
+                ])
+                ->addTag('assets.package', [
+                    'package' => $name
+                ])
+        );
+
+        /*
+         * add translations paths
+         */
+        $translationFolder = realpath($projectDir . '/vendor/roadiz/rozier/src/Resources/translations');
+        if ($container->hasDefinition('translator.default') && file_exists($translationFolder)) {
+            $translator = $container->findDefinition('translator.default');
+            $files = [];
+            $finder = Finder::create()
+                ->followLinks()
+                ->files()
+                ->filter(function (\SplFileInfo $file) {
+                    return 2 <= substr_count($file->getBasename(), '.') &&
+                        preg_match('/\.\w+$/', $file->getBasename());
+                })
+                ->in($translationFolder)
+                ->sortByName()
+            ;
+            foreach ($finder as $file) {
+                $fileNameParts = explode('.', basename((string) $file));
+                $locale = $fileNameParts[\count($fileNameParts) - 2];
+                if (!isset($files[$locale])) {
+                    $files[$locale] = [];
+                }
+
+                $files[$locale][] = (string) $file;
+            }
+            $options = array_merge_recursive(
+                $translator->getArgument(4),
+                [
+                    'resource_files' => $files,
+                    'scanned_directories' => $scannedDirectories = [$translationFolder],
+                    'cache_vary' => [
+                        'scanned_directories' => array_map(static function (string $dir) use ($projectDir): string {
+                            return str_starts_with($dir, $projectDir.'/') ? substr($dir, 1 + \strlen($projectDir)) : $dir;
+                        }, $scannedDirectories),
+                    ],
+                ]
+            );
+
+            $translator->replaceArgument(4, $options);
+        }
+    }
+}
--- src/RoadizRozierBundle.php
+++ src/RoadizRozierBundle.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\RozierBundle;
 
+use RZ\Roadiz\RozierBundle\DependencyInjection\Compiler\RozierPathsCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RoadizRozierBundle extends Bundle
@@ -10,5 +12,12 @@ class RoadizRozierBundle extends Bundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RozierPathsCompilerPass());
     }
 }
