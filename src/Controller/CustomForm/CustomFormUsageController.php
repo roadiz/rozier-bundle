9df4675febbd61405a3264c6c77768d3302bdf4f
Ambroise Maupate <ambroise@rezo-zero.com>
2022-07-22 12:06:45 +0200

    feat: Allow editing SEO info on non-reachable nodes

--- src/Controller/CustomForm/CustomFormUsageController.php
+++ src/Controller/CustomForm/CustomFormUsageController.php
@@ -4,41 +4,19 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\RozierBundle\Controller\CustomForm;
 
-use Doctrine\Persistence\ManagerRegistry;
 use RZ\Roadiz\CoreBundle\Entity\CustomForm;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Themes\Rozier\RozierApp;
 
 final class CustomFormUsageController extends RozierApp
 {
-    private ManagerRegistry $managerRegistry;
-
-    /**
-     * @param ManagerRegistry $managerRegistry
-     */
-    public function __construct(ManagerRegistry $managerRegistry)
-    {
-        $this->managerRegistry = $managerRegistry;
-    }
-
-    /**
-     * @param int $id
-     * @return Response
-     */
-    public function usageAction(int $id)
+    public function usageAction(CustomForm $id): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ACCESS_CUSTOMFORMS');
-        /** @var CustomForm|null customForm */
-        $customForm = $this->managerRegistry->getRepository(CustomForm::class)->find($id);
+        $customForm = $id;
+        $this->assignation['customForm'] = $customForm;
+        $this->assignation['usages'] = $customForm->getNodes();
 
-        if ($customForm !== null) {
-            $this->assignation['customForm'] = $customForm;
-            $this->assignation['usages'] = $customForm->getNodes();
-
-            return $this->render('@RoadizRozier/custom-forms/usage.html.twig', $this->assignation);
-        }
-
-        throw new ResourceNotFoundException();
+        return $this->render('@RoadizRozier/custom-forms/usage.html.twig', $this->assignation);
     }
 }
