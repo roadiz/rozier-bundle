21fd924bd5b158a8e1992ec6672ece1f6043aa34
roadiz-ci <ambroise+ci@roadiz.io>
2025-09-10 10:20:04 +0000

    chore: Bumped

--- src/Controller/Document/DocumentArchiveController.php
+++ src/Controller/Document/DocumentArchiveController.php
@@ -6,11 +6,8 @@ namespace RZ\Roadiz\RozierBundle\Controller\Document;
 
 use Doctrine\Persistence\ManagerRegistry;
 use League\Flysystem\FilesystemException;
-use Psr\Log\LoggerInterface;
 use RZ\Roadiz\CoreBundle\Entity\Document;
-use RZ\Roadiz\CoreBundle\Security\LogTrail;
 use RZ\Roadiz\Documents\DocumentArchiver;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,31 +16,32 @@ use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Themes\Rozier\RozierApp;
+use Twig\Error\RuntimeError;
 
-final class DocumentArchiveController extends AbstractController
+final class DocumentArchiveController extends RozierApp
 {
     public function __construct(
         private readonly ManagerRegistry $managerRegistry,
         private readonly TranslatorInterface $translator,
-        private readonly DocumentArchiver $documentArchiver,
-        private readonly LoggerInterface $logger,
-        private readonly LogTrail $logTrail,
+        private readonly DocumentArchiver $documentArchiver
     ) {
     }
 
     /**
+     * Return an deletion form for multiple docs.
+     *
+     * @param Request $request
+     *
+     * @return Response
      * @throws FilesystemException
+     * @throws RuntimeError
      */
     public function bulkDownloadAction(Request $request): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ACCESS_DOCUMENTS');
 
         $documentsIds = $request->get('documents', []);
-
-        if (is_string($documentsIds)) {
-            $documentsIds = json_decode($documentsIds, true, flags: JSON_THROW_ON_ERROR);
-        }
-
         if (!is_array($documentsIds) || count($documentsIds) <= 0) {
             throw new ResourceNotFoundException('No selected documents to download.');
         }
@@ -55,42 +53,41 @@ final class DocumentArchiveController extends AbstractController
                 'id' => $documentsIds,
             ]);
 
-        if (0 === count($documents)) {
-            throw new ResourceNotFoundException();
-        }
+        if (count($documents) > 0) {
+            $this->assignation['documents'] = $documents;
+            $form = $this->buildBulkDownloadForm($documentsIds);
+            $form->handleRequest($request);
 
-        $assignation = [];
-        $assignation['documents'] = $documents;
-        $form = $this->buildBulkDownloadForm($documentsIds);
-        $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                try {
+                    return $this->documentArchiver->archiveAndServe($documents, 'Documents archive');
+                } catch (\Exception $e) {
+                    $this->getLogger()->error($e->getMessage());
+                    $msg = $this->translator->trans('documents.cannot_download');
+                    $this->publishErrorMessage($request, $msg);
+                }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            try {
-                return $this->documentArchiver->archiveAndServe($documents, 'Documents archive');
-            } catch (\Exception $e) {
-                $this->logger->error($e->getMessage());
-                $msg = $this->translator->trans('documents.cannot_download');
-                $this->logTrail->publishErrorMessage($request, $msg);
+                return $this->redirectToRoute('documentsHomePage');
             }
 
-            return $this->redirectToRoute('documentsHomePage');
+            $this->assignation['form'] = $form->createView();
+            $this->assignation['action'] = '?' . http_build_query(['documents' => $documentsIds]);
+            $this->assignation['thumbnailFormat'] = [
+                'quality' => 50,
+                'fit' => '128x128',
+                'sharpen' => 5,
+                'inline' => false,
+                'picture' => true,
+                'controls' => false,
+                'loading' => 'lazy',
+            ];
+
+            return $this->render('@RoadizRozier/documents/bulkDownload.html.twig', $this->assignation);
         }
 
-        $assignation['form'] = $form->createView();
-        $assignation['action'] = '?'.http_build_query(['documents' => $documentsIds]);
-        $assignation['thumbnailFormat'] = [
-            'quality' => 50,
-            'crop' => '1:1',
-            'width' => 128,
-            'sharpen' => 5,
-            'inline' => false,
-            'picture' => true,
-            'controls' => false,
-            'loading' => 'lazy',
-        ];
-
-        return $this->render('@RoadizRozier/documents/bulkDownload.html.twig', $assignation);
+        throw new ResourceNotFoundException();
     }
+
 
     private function buildBulkDownloadForm(array $documentsIds): FormInterface
     {
@@ -98,7 +95,7 @@ final class DocumentArchiveController extends AbstractController
             'checksum' => md5(serialize($documentsIds)),
         ];
         $builder = $this->createFormBuilder($defaults, [
-            'action' => '?'.http_build_query(['documents' => $documentsIds]),
+            'action' => '?' . http_build_query(['documents' => $documentsIds]),
         ])
             ->add('checksum', HiddenType::class, [
                 'constraints' => [
